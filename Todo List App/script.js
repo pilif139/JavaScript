14b4e7b2651604a63be89c39cd83470959a93cfd
pilif139 <pifilif@gmail.com>
2024-04-26 20:49:19 +0200

    fixed bugs

--- Todo List App/script.js
+++ Todo List App/script.js
@@ -1,29 +1,35 @@
 let btn = document.querySelector("#submitBtn");
 let input = document.querySelector("#todo");
 
-function edit(obj){
+function edit(parent){
     let x;
-    if(obj.classList.contains("editing")){
-        obj.classList.remove("editing")
-        x = obj.childNodes[0].value;
-        obj.innerHTML = `${x} <div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
+    if(parent.classList.contains("editing")){
+        parent.classList.remove("editing")
+        x = parent.childNodes[0].childNodes[0].value;
+        if(x== ''){
+            x="No Task";
+        }
+        parent.innerHTML = `<div>${x}</div> <div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement,this)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
+        
         
     }
-    else{
-        let val = obj.textContent;
-        obj.innerHTML = `<input type="text" value=${val}> <div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
-        obj.classList.add("editing")
+    else if(!parent.classList.contains("removed_item")){
+        let val = parent.textContent;
+        parent.innerHTML = `<div><input type="text" value=${val}> 
+        </div><div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement,this)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
+        parent.classList.add("editing")
     }
 }
 
-function remove(obj){
-    if(obj.classList.contains("removed_item")){
-        obj.classList.remove("removed_item")
-        obj.childNodes[1].childNodes[0].textContent = "REMOVE"
+function remove(parent, button){
+    if(parent.classList.contains("removed_item")){
+        parent.classList.remove("removed_item")
+        button.textContent = "REMOVE"
     }
-    else{
-        obj.classList.add("removed_item")
-        obj.childNodes[1].childNodes[0].textContent = "UNREMOVE"
+    else if(!parent.classList.contains("editing")){ 
+        parent.classList.add("removed_item")
+
+        button.textContent = "UNREMOVE"
     }
 }
 
@@ -32,17 +38,17 @@ btn.addEventListener("click", function(){
     if(input.value == ''){
         input.classList.add("error");
     }else{
+        document.querySelector("main > h2").style.display = "block";
         input.classList.remove("error");
         let li = document.createElement("div");
         li.classList.add("todo_element");
-        li.innerHTML = `${input.value} <div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
+        li.innerHTML = `<div>${input.value}</div> <div><button type="button" class="removeLi" onclick="remove(this.parentElement.parentElement, this)">REMOVE</button> <button type="button"  class="editLi" onclick="edit(this.parentElement.parentElement)">EDIT</button></div>`
         document.querySelector("#todo_list").append(li);
 
-        //jquery
+        //jquery nie dziala
         li.style.display = "none";
-        li.id = "id1";
-        $("#id1").fadeIn(250);
-        li.removeAttribute("id");
+        jQuery(li).slideDown(250, console.log("done"));
+        jQuery("main > h2").slideDown(250)
     }
 });
 
